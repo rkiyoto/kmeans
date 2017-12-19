3f67e4c32f571e6f71458938a8b1ba85ceda83c9
Otho <marcelo.lopes.lotufo@gmail.com>
2017-12-18 22:02:56 -0200

    Breast cancer

--- kmeans.py
+++ kmeans.py
@@ -1,17 +1,60 @@
+# matplotlib inline
+from copy import deepcopy
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
-
+import pandas as pd
+from matplotlib import pyplot as plt
 plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
 
-# Creating a sample dataset with 4 clusters
-X, y = make_blobs(n_samples=800, n_features=3, centers=4)
+# Importing the dataset
+data = pd.read_csv('breast_cancer.csv')
+print(data.shape)
+data.head()
 
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.scatter(X[:, 0], X[:, 1], X[:, 2])
+# Getting the values and plotting it
+f1 = data['malignant'].values
+f2 = data['benign'].values
+X = np.array(list(zip(f1, f2)))
+
+# Euclidean Distance Caculator
+def dist(a, b, ax=1):
+    return np.linalg.norm(a - b, axis=ax)
+
+# Number of clusters
+k = 2
+# X coordinates of random centroids
+C_x = np.random.random(size=k)
+# Y coordinates of random centroids
+C_y = np.random.random(size=k)
+C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
+
+# To store the value of centroids when it updates
+C_old = np.zeros(C.shape)
+# Cluster Lables(0, 1, 2)
+clusters = np.zeros(len(X))
+# Error func. - Distance between new centroids and old centroids
+error = dist(C, C_old, None)
+# Loop will run till the error becomes zero
+while error != 0:
+    # Assigning each value to its closest cluster
+    for i in range(len(X)):
+        distances = dist(X[i], C)
+        cluster = np.argmin(distances)
+        clusters[i] = cluster
+    # Storing the old centroid values
+    C_old = deepcopy(C)
+    # Finding the new centroids by taking the average value
+    for i in range(k):
+        points = [X[j] for j in range(len(X)) if clusters[j] == i]
+        C[i] = np.mean(points, axis=0)
+    error = dist(C, C_old, None)
+
+colors = ['r', 'g', 'b', 'y', 'c', 'm']
+fig, ax = plt.subplots()
+for i in range(k):
+        points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
+        ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
+ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
 
 import pylab
 pylab.show()
